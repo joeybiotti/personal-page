f1a6463ca8f72188d00a5166dbc190020c72f34b
Joey Biotti <joey@MacBook-Pro-2.local>
2017-10-04 23:00:01 -0500

    moved the jquery around a bit

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,4 @@
-$(".tech-img").hover(
+$(".projTitle").hover(
     function(){$(this).animate({width: 400, height:400}, 1000);},        
     function(){$(this).animate({width: 200, height:200}, 1000);}
 );
