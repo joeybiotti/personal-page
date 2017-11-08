a47c8e911c8a48b280283caa3ab16388db2a4498
Joey Biotti <joey@MacBook-Pro-2.local>
2017-11-08 17:57:09 -0600

    added fadein to navbar

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,3 +2,15 @@ $(".projectImg").hover(
     function(){$(this).animate({width: 400, height:400});},
     function(){$(this).animate({width: 400, height:400});}
 );
+
+$(document).ready(function(){
+       $(window).bind('scroll', function() {
+       var distance = 50;
+             if ($(window).scrollTop() > distance) {
+                 $('nav').fadeIn(500);
+             }
+             else {
+                 $('nav').fadeOut(500);
+             }
+        });
+    });
