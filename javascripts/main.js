3562a8d4446bf06ac9a760b8b5febbb063668e27
Joey Biotti <joey@MacBook-Pro-2.local>
2018-01-04 20:43:51 -0600

    began style/content overhaul

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,13 +1,13 @@
 // navbar fades on scroll
 
- $(document).ready(function() {
+//  $(document).ready(function() {
 
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > 280) {
-      $('#nav').removeClass('navbar-trans').addClass('navbar-fixed');
-    }
-    if ($(window).scrollTop() < 281) {
-      $('#nav').removeClass('navbar-fixed').addClass('navbar-trans');
-    }
-  });
-});
+//   $(window).scroll(function () {
+//     if ($(window).scrollTop() > 280) {
+//       $('#nav').removeClass('navbar-trans').addClass('navbar-fixed');
+//     }
+//     if ($(window).scrollTop() < 281) {
+//       $('#nav').removeClass('navbar-fixed').addClass('navbar-trans');
+//     }
+//   });
+// });
