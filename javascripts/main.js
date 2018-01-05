3f3a6962f83d379658376c95c2c39813aedd16df
Joey Biotti <joey@MacBook-Pro-2.local>
2018-01-04 23:32:46 -0600

    navbar and layout changes

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,13 +1,1 @@
-// navbar fades on scroll
-
-//  $(document).ready(function() {
-
-//   $(window).scroll(function () {
-//     if ($(window).scrollTop() > 280) {
-//       $('#nav').removeClass('navbar-trans').addClass('navbar-fixed');
-//     }
-//     if ($(window).scrollTop() < 281) {
-//       $('#nav').removeClass('navbar-fixed').addClass('navbar-trans');
-//     }
-//   });
-// });
+console.log('main.js loaded');
